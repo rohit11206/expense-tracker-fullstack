d02d06ed490e2699c06ceffa701b501421137c18
ROhit <rohitvp11206chauhan@gmail.com>
2026-06-05 17:04:06 +0530

    fix: use hash router for production routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { ToastProvider } from "@/context/ToastContext";
 import { ToastContainer } from "@/components/common/Toast";
 import { AppRouter } from "@/routes/AppRouter";
 
+
 export default function App() {
   return (
     <ThemeProvider>
